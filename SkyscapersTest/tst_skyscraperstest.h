09e1fbb58d959c38d27cf3064acb49dd87ddb717
SandroWissmann <sandro.wissmann@gmail.com>
2021-02-16 16:36:53 +0100

    test: adds tests for 8x8 skyscraper

--- SkyscapersTest/tst_skyscraperstest.h
+++ SkyscapersTest/tst_skyscraperstest.h
@@ -373,4 +373,69 @@ TEST(Skyscrapers, sky7_partial_2)
               expectedResult);
 }
 
+TEST(Skyscrapers, sky8_easy_partial)
+{
+    std::vector<int> clues = {1, 5, 2, 2, 3, 2, 3, 4, 3, 3, 3, 2, 3, 4, 2, 1,
+                              1, 3, 2, 2, 4, 5, 3, 2, 2, 4, 2, 3, 3, 4, 2, 1};
+
+    std::vector<std::vector<int>> startingBoard{
+        {0, 0, 0, 4, 0, 0, 0, 0}, {0, 0, 0, 0, 3, 0, 7, 0},
+        {3, 0, 0, 0, 0, 0, 0, 2}, {0, 1, 0, 0, 0, 0, 0, 3},
+        {0, 0, 6, 0, 0, 8, 5, 0}, {0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 2, 0, 0, 0, 0, 0}, {0, 2, 0, 0, 0, 0, 4, 0},
+    };
+
+    std::vector<std::vector<int>> expectedResult = {
+        {8, 3, 1, 4, 6, 7, 2, 5}, {5, 4, 8, 2, 3, 1, 7, 6},
+        {3, 6, 7, 8, 4, 5, 1, 2}, {6, 1, 4, 7, 5, 2, 8, 3},
+        {4, 7, 6, 3, 2, 8, 5, 1}, {2, 8, 5, 1, 7, 3, 6, 4},
+        {1, 5, 2, 6, 8, 4, 3, 7}, {7, 2, 3, 5, 1, 6, 4, 8},
+    };
+
+    EXPECT_EQ(SolvePuzzle(clues, startingBoard, startingBoard.size()),
+              expectedResult);
+}
+
+TEST(Skyscrapers, sky8_medium_partial)
+{
+    std::vector<int> clues = {3, 3, 3, 2, 1, 4, 3, 3, 3, 4, 2, 1, 3, 3, 3, 2,
+                              4, 1, 2, 5, 2, 3, 2, 3, 3, 2, 2, 1, 4, 2, 2, 3};
+
+    std::vector<std::vector<int>> startingBoard{
+        {0, 4, 0, 0, 0, 0, 0, 0}, {0, 3, 0, 0, 0, 0, 2, 0},
+        {3, 0, 0, 0, 0, 5, 0, 0}, {0, 6, 0, 3, 0, 0, 0, 0},
+        {0, 0, 0, 0, 7, 0, 0, 0}, {1, 0, 0, 0, 0, 2, 0, 0},
+        {0, 0, 0, 4, 0, 0, 7, 5}, {0, 0, 0, 0, 0, 4, 0, 0}};
+
+    std::vector<std::vector<int>> expectedResult = {
+        {5, 4, 3, 7, 8, 1, 6, 2}, {7, 3, 4, 8, 5, 6, 2, 1},
+        {3, 2, 8, 1, 6, 5, 4, 7}, {4, 6, 1, 3, 2, 7, 5, 8},
+        {8, 5, 6, 2, 7, 3, 1, 4}, {1, 8, 7, 5, 4, 2, 3, 6},
+        {6, 1, 2, 4, 3, 8, 7, 5}, {2, 7, 5, 6, 1, 4, 8, 3}};
+
+    EXPECT_EQ(SolvePuzzle(clues, startingBoard, startingBoard.size()),
+              expectedResult);
+}
+
+TEST(Skyscrapers, sky8_hard_partial)
+{
+    std::vector<int> clues = {3, 0, 4, 3, 3, 0, 3, 2, 0, 0, 2, 4, 0, 4, 1, 5,
+                              0, 4, 0, 3, 0, 0, 4, 3, 0, 0, 0, 3, 3, 4, 0, 3};
+
+    std::vector<std::vector<int>> startingBoard{
+        {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 5, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 4},
+        {0, 0, 1, 6, 5, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 3, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1}};
+
+    std::vector<std::vector<int>> expectedResult = {
+        {1, 6, 2, 4, 3, 8, 5, 7}, {6, 8, 5, 2, 4, 1, 7, 3},
+        {2, 3, 7, 5, 8, 4, 1, 6}, {3, 1, 6, 8, 7, 5, 2, 4},
+        {4, 7, 1, 6, 5, 3, 8, 2}, {8, 2, 4, 3, 1, 7, 6, 5},
+        {7, 5, 3, 1, 2, 6, 4, 8}, {5, 4, 8, 7, 6, 2, 3, 1}};
+
+    EXPECT_EQ(SolvePuzzle(clues, startingBoard, startingBoard.size()),
+              expectedResult);
+}
+
 #endif // TST_SKYSCRAPERETEST_H
