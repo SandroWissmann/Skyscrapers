c69cc327fcddcb68272d5cca77dcd7441edaf0b9
SandroWissmann <sandro.wissmann@gmail.com>
2021-03-12 15:35:15 +0100

    feat: adds dummy backtracking algorithm

--- Skyscrapers/backtracking.cpp
+++ Skyscrapers/backtracking.cpp
@@ -7,6 +7,41 @@
 
 namespace backtracking {
 
+bool isValid()
+{
+    return true;
+}
+
+bool insertNextSkyscraper(Board &board, std::size_t x, std::size_t y,
+                          std::size_t size)
+{
+    if (x == size) {
+        x = 0;
+        y++;
+    };
+    if (y == size) {
+        return true;
+    }
+    if (board.skyscrapers[y][x] != 0) { // skyscraper present
+        // invalid step?
+        // -> return false
+        return insertNextSkyscraper(board, x + 1, y, size);
+    }
+
+    for (board.skyscrapers[y][x] = 1;
+         board.skyscrapers[y][x] <= static_cast<int>(board.skyscrapers.size());
+         ++board.skyscrapers[y][x]) {
+
+        // if invalid continue;
+
+        if (insertNextSkyscraper(board, x + 1, y, size)) {
+            return true;
+        }
+    }
+    board.skyscrapers[y][x] = 0;
+    return false;
+}
+
 std::vector<std::vector<int>>
 SolvePuzzle(const std::vector<int> &clues,
             std::vector<std::vector<int>> startingGrid, int)
@@ -26,7 +61,11 @@ SolvePuzzle(const std::vector<int> &clues,
         return board.skyscrapers;
     }
 
-    // debug_print(board);
+    debug_print(board);
+
+    insertNextSkyscraper(board, 0, 0, board.skyscrapers.size());
+
+    debug_print(board);
 
     return board.skyscrapers;
 }
